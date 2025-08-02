462eb5f313262bee75512efe0cf09a7c17cc3dc3
Maximiliano Meriño <maximiliano.paredes.enrique@gmail.com>
2025-08-01 21:11:27 -0400

    modificacion crear_usuario

--- crear_usuario.py
+++ crear_usuario.py
@@ -1,11 +1,11 @@
 from app import app, db, User
 
 with app.app_context():
-    if not User.query.filter_by(username='maxi').first():
-        nuevo_usuario = User(username='maxi', role='mecanico')
-        nuevo_usuario.set_password('1234')
+    if not User.query.filter_by(username='admin').first():
+        nuevo_usuario = User(username='admin', role='admin')
+        nuevo_usuario.set_password('adminpass2025')
         db.session.add(nuevo_usuario)
         db.session.commit()
-        print("Usuario 'maxi' creado con contraseña '1234'")
+        print("Usuario 'admin' creado con contraseña 'adminpass2025'")
     else:
-        print("El usuario 'maxi' ya existe")
+        print("El usuario 'admin' ya existe")
